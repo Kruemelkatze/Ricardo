2160b0bd19cf4753de380b59c6fde2b4f9aef921
Fabian Schober <fabian@imendo.at>
2019-09-27 19:17:51 +0200

    Blur BG works

---
+++ Assets/Scripts/Background/FillScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale =
+            new Vector3(Camera.main.orthographicSize * 2.0f * Camera.main.pixelWidth / Camera.main.pixelHeight,
+                Camera.main.orthographicSize * 2.0f, 0.1f);
+    }
+}
